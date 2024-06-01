7785bd8191d61b7044919a18a354a433bb432177
gun <ydk2001524@gmail.com>
2024-06-01 14:17:04 +0900

    회원가입 기능 구현

--- src/main/java/com/example/TeamProject/Person.java
+++ src/main/java/com/example/TeamProject/Person.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 @NoArgsConstructor@AllArgsConstructor
 @Getter@Setter@ToString
+@Builder
 public class Person {
     String id;
     String pw;
--- src/main/java/com/example/TeamProject/PersonController.java
+++ src/main/java/com/example/TeamProject/PersonController.java
@@ -2,21 +2,40 @@ package com.example.TeamProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 public class PersonController {
 
-    @RequestMapping("/")
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping("/") // 메인(로그인)화면
     public String Main() {
         return "Main";
     }
 
-    @RequestMapping("/register")
-    public String register() {
+    @RequestMapping("/register") // 폼 표시
+    public String registerFrom(Model model) {
+        model.addAttribute("person", new Person());
         return "register";
+    }
+
+    @RequestMapping("/register") // 가입 정보 저장
+    public String registerSubmit(@ModelAttribute Person person, Model model) {
+        PersonService.addUser(person);
+        model.addAttribute("person", person);
+        return "result";
+    }
+
+    @RequestMapping("/login") // 도서 리스트에서 00님 환영합니다 표시
+    @ResponseBody
+    public String login(@RequestParam("id") String id) {
+        return id+"님 환영합니다!";
     }
 
 
