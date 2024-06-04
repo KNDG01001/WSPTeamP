91625d198e3fa0f95bc046d21bdddc1aa30f7821
최용석 <phone13324@gmail.com>
2024-06-05 07:30:58 +0900

    이미 빌린 책이면 중복되어서 빌리지 않도록

--- src/main/java/com/example/TeamProject/Borrow/BorrowServiceImpl.java
+++ src/main/java/com/example/TeamProject/Borrow/BorrowServiceImpl.java
@@ -33,15 +33,18 @@ public class BorrowServiceImpl implements BorrowService {
     public void borrowBook(String userId, long bookId) {
         Person user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다"));
         Book book = bookRepository.findById(bookId).orElseThrow(() -> new RuntimeException("책을 찾을 수 없습니다"));
-
-        //borrowdto에 넘겨진 값 저장
-        BorrowDTO borrowDTO = new BorrowDTO();
-        borrowDTO.setUserId(userId);
-        borrowDTO.setBookId(bookId);
-        borrowDTO.setBorrowDate(new Date()); //대출은 오늘 날짜로
-        borrowDTO.setReturnDate(returnDate); //일주일 뒤 반납
-        Borrow borrow = borrowDTO.toEntity(user, book); // Entitiy로 변환해서
-        borrowRepository.save(borrow); // db에 저장
+        Borrow access = borrowRepository.findByUserUserIdAndBookBookId(userId, bookId); // 해당 유저ID가 빌린 책 ID로 찾아서 이미 이 책을 빌렸으면 다시 못빌리게
+        if(access!=null){ //이미 있으면
+            System.out.println("Already Borrowed");
+        }else{ //borrowdto에 넘겨진 값 저장
+            BorrowDTO borrowDTO = new BorrowDTO();
+            borrowDTO.setUserId(userId);
+            borrowDTO.setBookId(bookId);
+            borrowDTO.setBorrowDate(new Date()); //대출은 오늘 날짜로
+            borrowDTO.setReturnDate(returnDate); //일주일 뒤 반납
+            access = borrowDTO.toEntity(user, book); // Entitiy로 변환해서
+            borrowRepository.save(access); // db에 저장
+        }
     }
     @Override // 반납 구현
     public void returnBook(String userId, long bookId) {
