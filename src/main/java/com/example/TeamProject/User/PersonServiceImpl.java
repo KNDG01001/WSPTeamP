9dcd857afd984ae8788d4fd1e427fe4e276ad479
최용석 <phone13324@gmail.com>
2024-06-05 15:13:18 +0900

    Person findAll() 삭제

--- src/main/java/com/example/TeamProject/User/PersonServiceImpl.java
+++ src/main/java/com/example/TeamProject/User/PersonServiceImpl.java
@@ -14,13 +14,6 @@ public class PersonServiceImpl implements PersonService {
     private PersonRepository repository;
 
     @Override
-    public List<PersonDTO> findAll() {
-        return repository.findAll().stream()
-                .map(PersonDTO::new)
-                .collect(Collectors.toList());
-    }
-
-    @Override
     public void addUser(PersonDTO personDTO) {
         Person person = personDTO.toEntity();
         repository.save(person);
@@ -28,14 +21,12 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     public boolean validateUser(String userId, String userPw) {
-        System.out.println(repository.findById(userId));
         if (repository.findById(userId).isPresent()) {
-            System.out.println("good");
             Person person = repository.findById(userId).get();
                 return person.getUserPw().equals(userPw);
         }
         else{
-        return false;
-    }
+            return false;
+        }
     }
 }
