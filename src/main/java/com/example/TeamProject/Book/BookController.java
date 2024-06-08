f5eb4ed0da514818fdc763e198909b21bd74beb8
최용석 <phone13324@gmail.com>
2024-06-08 17:25:04 +0900

    테스트용 /list 삭제, book 수정 추가(BookEdit)

--- src/main/java/com/example/TeamProject/Book/BookController.java
+++ src/main/java/com/example/TeamProject/Book/BookController.java
@@ -14,31 +14,45 @@ import java.util.List;
 public class BookController {
 
     @Autowired
-    private BookService BookService;
+    private BookService bookService;
     @Autowired
     private ReviewService reviewService;
 
     @RequestMapping("/BookList")
-    public  String BookList(Model model) {
-        model.addAttribute("books",BookService.findAll());
+    public String BookList(Model model) {
+        model.addAttribute("books", bookService.findAll());
         return "BookList";
     }
-    @RequestMapping("/list")  @ResponseBody
-    public String list() {
-    return "목록 : " + BookService.findAll();
-}
 
     @RequestMapping("/BorrowList")
-    public String BorrowList(Model model){
+    public String BorrowList(Model model) {
         return "";
     }
 
     @RequestMapping("/BookDetail/{bookId}")
     public String BookDetail(@PathVariable("bookId") long bookId, Model model) {
-            model.addAttribute("book", BookService.findById(bookId));
-            List<Review> reviews = reviewService.findReviewsByBookId(bookId);
-            model.addAttribute("reviews", reviews);
-            return "BookDetail";
+        model.addAttribute("book", bookService.findById(bookId));
+        List<Review> reviews = reviewService.findReviewsByBookId(bookId);
+        model.addAttribute("reviews", reviews);
+        return "BookDetail";
     }
 
+    @PostMapping("/BookEdit")
+    public String editBook(@RequestParam long bookId, @RequestParam String author,
+                           @RequestParam String title, @RequestParam String image,
+                           @RequestParam String detail) {
+        BookDTO book = bookService.findById(bookId);
+        book.setAuthor(author);
+        book.setTitle(title);
+        book.setImage(image);
+        book.setDetail(detail);
+        bookService.updateBook(book);
+        return "redirect:/BookDetail/" + bookId;
+    }
+
+    @GetMapping("/BookEdit")
+    public String bookEdit(@RequestParam long bookId, Model model) {
+        model.addAttribute("book", bookService.findById(bookId));
+        return "BookEdit";
+    }
 }
